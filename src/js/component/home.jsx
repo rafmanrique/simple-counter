1423a44265a6785a159addc10e210ede7da82546
rafmanrique <rafamanrique26@gmail.com>
2022-03-23 21:24:18 +0000

    Simple-Counter Made

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container d-flex justify-content-center">
+				<div className="numberContainer d-flex justify-content-center p-4 bg-dark text-light rounded w-100">
+					<div className="iconContainer px-3 display-2">
+						<FontAwesomeIcon icon={faClock} />
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitSix % 10}
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitFive % 10}
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitFour % 10}
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitThree % 10}
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitTwo % 10}
+					</div>
+					<div className="digit px-3 mx-3 display-2 border border-light rounded">
+						{props.digitOne % 10}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
